bdb29cac68abf7f653ab4098d0d141edbbb757e6
mudrol <matias.hiillos@gmail.com>
2026-06-22 20:48:05 +1000

    Fix relative path bug in save_raw()
    Replace relative data with absolute project-root path to ensure correct file creation under cron and system daemons.
    Add log files to .gitignore.

--- src/ingestion/fetch_latest.py
+++ src/ingestion/fetch_latest.py
@@ -10,10 +10,11 @@ def save_raw(data):
   timestamp = datetime.now(timezone.utc)
   date_path = timestamp.strftime("%Y-%m-%d")
   fname = timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
-  file_path = Path(f"data/raw/{date_path}/{fname}.json")
-  file_path.parent.mkdir(parents=True, exist_ok=True)
+  base_path = Path(__file__).resolve().parents[2]
+  full_path = base_path / "data" / "raw" / date_path / f"{fname}.json"
+  full_path.parent.mkdir(parents=True, exist_ok=True)
 
-  with open(file_path, "w") as f:
+  with open(full_path, "w") as f:
     json.dump(data, f)
 
   sys.stdout.write(f"[gem] fetched latest data at {fname}.\n")
@@ -25,8 +26,8 @@ def main():
     data = get_latest_prices()
     if not data or "data" not in data:
       raise ValueError("Invalid API rseponse")
-
     save_raw(data)
+
   except Exception as e:
     sys.stderr.write(f"[gem] error while fetching latest prices: {e}\n")
 
