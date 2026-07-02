37fb5cb76073ffc2b234c38ac3222a9f826cd977
mudrol <matias.hiillos@gmail.com>
2026-07-02 20:24:12 +1000

    Update ge_client.py to optionally cache mapping
    Introduce an optional flag update_mapping which by default will read the mapping data from cache.
    This is done to prevent extensive API calls when running the pipeline.

--- src/ingestion/ge_client.py
+++ src/ingestion/ge_client.py
@@ -1,10 +1,15 @@
 import requests
+import json
+import sys
+from pathlib import Path
 
 BASE_URL = "https://prices.runescape.wiki/api/v1/osrs"
 
 HEADERS = {
     "User-Agent": "gem - github.com/mhiillos/gem"
     }
+
+MAPPING_CACHE = Path(__file__).parents[2] / "data" / "cache" / "mapping.json"
 
 # Fetches the latest price information for every item, or for a single item given by ID.
 def get_latest_prices(id = None):
@@ -17,11 +22,19 @@ def get_latest_prices(id = None):
   return response.json()
 
 # Fetches list of objects mapping the ID with name and other information.
-def get_mapping():
+def get_mapping(update_mapping=False):
   url = f"{BASE_URL}/mapping"
-  response = requests.get(url, headers=HEADERS)
-  response.raise_for_status()
-  return response.json()
+  if not MAPPING_CACHE.exists() or update_mapping:
+    sys.stdout.write("[gem] fetching new mapping from api...")
+    response = requests.get(url, headers=HEADERS)
+    response.raise_for_status()
+
+    MAPPING_CACHE.parent.mkdir(parents=True, exist_ok=True)
+    MAPPING_CACHE.write_text(json.dumps(response.json()))
+    sys.stdout.write("ok\n")
+    return response.json()
+  else:
+    return json.loads(MAPPING_CACHE.read_text())
 
 # Fetches the timeseries data of the given item by id and time-interval
 def get_timeseries(id, timestep):
