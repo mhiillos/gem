7b18d3892a59e75c9215f5c3a38a83d8aa2ea54b
mudrol <matias.hiillos@gmail.com>
2026-07-26 22:02:42 +1000

    Update backfill to ignore old data
    Update backfill.py to check the latest database timestamp before processing old snapshots.
    Add src/db/queries.py with latest_timestamp() helper.

--- scripts/backfill.py
+++ scripts/backfill.py
@@ -2,11 +2,19 @@
 
 from pathlib import Path
 from scripts.run_pipeline import run_pipeline
+from src.db.queries import latest_timestamp
+from datetime import datetime, timezone
 
 def main():
   base_path = Path(__file__).resolve().parents[1] / "data" / "raw"
-  for file_path in base_path.rglob("*.json"):
-    print(f"Processing {file_path.name}")
+  latest_ts = latest_timestamp()
+  for file_path in sorted(base_path.rglob("*.json")):
+    rawfile_ts = datetime.strptime(file_path.stem, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc).timestamp()
+    if rawfile_ts <= latest_ts:
+      print(f"[gem] database contains newer or equal data than {file_path.name}, skipping.")
+      continue
+
+    print(f"[gem] Processing {file_path.name}")
     run_pipeline(file_path)
 
 if __name__ == "__main__":
---
+++ src/db/queries.py
@@ -0,0 +1,17 @@
+from src.db.connection import get_connection
+
+def latest_timestamp():
+  query = """
+  SELECT MAX(timestamp)
+  FROM fact_item
+  """
+  with get_connection() as conn:
+    with conn.cursor() as cur:
+      cur.execute(query)
+      response = cur.fetchone()[0]
+
+      if response is None:
+        return 0
+
+      return response.timestamp()
+
